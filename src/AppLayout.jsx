87e719fd08fbd1a3c038c8a38316daab1919e18a
Josh Wang <Joshw9012@gmail.com>
2023-12-11 20:31:05 +1100

    before- restructure 2

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,20 +1,12 @@
 import { useSelector } from "react-redux";
-import { Header, SideBar, Main, Footer } from "./App";
+import { Header } from "./Header";
+import { SideBar } from "./SideBar";
+import { Main } from "./Main.1";
+import { Footer } from "./Footer";
 
 export function AppLayout() {
-  // const page = useEffect(
-  //   function () {
-  //     if (currentPage) {
-  //       const todoList = useSelector((state) => state.todo);
-  //     }
-  //     if (!currentPage) {
-  //       const todoList = useSelector((state) => state.todo.status==="completed");
-  //   },
-  //   [currentPage]
-  // );
   const taskList = useSelector((state) => state.todo);
   const taskWatchList = taskList.filter((task) => task.status === true);
-  const completedList = taskList.filter((task) => task.status === false);
   const watchListNumItems = taskWatchList.length;
   const completedNumItems = taskList.reduce(
     (acc, task) => acc + (task.status === false ? 1 : 0),

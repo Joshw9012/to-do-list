c54a308d41388ed3821502803a290a212929c570
Josh Wang <Joshw9012@gmail.com>
2023-12-11 20:51:00 +1100

    Final version -updated initial data

--- src/components/todoSlice.js
+++ src/components/todoSlice.js
@@ -4,43 +4,50 @@ const initialState = [
   {
     id: 4210,
     duedate: "2024-01-01",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
+    description: "Learn React!",
     status: true, // "true: Tasks on-hand,  false: completed tasks
     priority: "Low",
   },
   {
     id: 41253,
     duedate: "2024-01-02",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
-    status: false, // "true: Tasks on-hand,  false: completed tasks
+    description: "Learn TypeScript",
+    status: true, // "true: Tasks on-hand,  false: completed tasks
     priority: "High",
   },
   {
     id: 12101,
     duedate: "2024-01-03",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
+    description: "Review .NetCore",
     status: true, // "true: Tasks on-hand,  false: completed tasks
     priority: "Medium",
   },
   {
     id: 156842,
     duedate: "2024-01-04",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
+    description: "Find a job",
     status: true, // "true: Tasks on-hand,  false: completed tasks
     priority: "Low",
   },
   {
     id: 54125,
     duedate: "2024-01-05",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
-    status: false, // "true: Tasks on-hand,  false: completed tasks
+    description: "become senior IT developer",
+    status: true, // "true: Tasks on-hand,  false: completed tasks
     priority: "High",
   },
   {
     id: 153783,
-    duedate: "2024-01-03",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
-    status: true, // "true: Tasks on-hand,  false: completed tasks
+    duedate: "2023-12-01",
+    description: "CPA qualified",
+    status: false, // "true: Tasks on-hand,  false: completed tasks
+    priority: "Medium",
+  },
+  {
+    id: 153783,
+    duedate: "2023-07-01",
+    description: "TAFE NSW - programming",
+    status: false, // "true: Tasks on-hand,  false: completed tasks
     priority: "Medium",
   },
 ];
